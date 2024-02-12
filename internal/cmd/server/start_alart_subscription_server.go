7856ab3dd7852bbf5568f15c35ab1810cb91f703
jenchen <jenchen@redhat.com>
2024-02-12 18:54:40 +0000

    start point of alert subscription

---
+++ internal/cmd/server/start_alart_subscription_server.go
@@ -0,0 +1,318 @@
+/*
+Copyright 2023 Red Hat Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is
+distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the License for the specific language governing permissions and limitations under the
+License.
+*/
+
+package server
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/spf13/cobra"
+
+	"github.com/openshift-kni/oran-o2ims/internal"
+	"github.com/openshift-kni/oran-o2ims/internal/authentication"
+	"github.com/openshift-kni/oran-o2ims/internal/authorization"
+	"github.com/openshift-kni/oran-o2ims/internal/exit"
+	"github.com/openshift-kni/oran-o2ims/internal/logging"
+	"github.com/openshift-kni/oran-o2ims/internal/network"
+	"github.com/openshift-kni/oran-o2ims/internal/service"
+)
+
+// Server creates and returns the `start deployment-manager-server` command.
+func AlertSubscriptionServer() *cobra.Command {
+	c := NewAlertSubscriptionServer()
+	result := &cobra.Command{
+		Use:   "deployment-manager-server",
+		Short: "Starts the deployment manager server",
+		Args:  cobra.NoArgs,
+		RunE:  c.run,
+	}
+	flags := result.Flags()
+	authentication.AddFlags(flags)
+	authorization.AddFlags(flags)
+	network.AddListenerFlags(flags, network.APIListener, network.APIAddress)
+	_ = flags.String(
+		cloudIDFlagName,
+		"",
+		"O-Cloud identifier.",
+	)
+	_ = flags.String(
+		backendURLFlagName,
+		"",
+		"URL of the backend server.",
+	)
+	_ = flags.String(
+		backendTypeFlagName,
+		string(service.DeploymentManagerBackendTypeRegularHub),
+		fmt.Sprintf(
+			"Type of backend. Possible values are '%s' and '%s'.",
+			service.DeploymentManagerBackendTypeGlobalHub,
+			service.DeploymentManagerBackendTypeRegularHub,
+		),
+	)
+	_ = flags.String(
+		backendTokenFlagName,
+		"",
+		"Token for authenticating to the backend server.",
+	)
+	_ = flags.StringArray(
+		extensionsFlagName,
+		[]string{},
+		"Extension to add to deployment managers.",
+	)
+	return result
+}
+
+// DeploymentManagerServerCommand contains the data and logic needed to run the `start
+// deployment-manager-server` command.
+type DeploymentManagerServerCommand struct {
+}
+
+// NewDeploymentManagerServer creates a new runner that knows how to execute the `start
+// deployment-manager-server` command.
+func NewDeploymentManagerServer() *DeploymentManagerServerCommand {
+	return &DeploymentManagerServerCommand{}
+}
+
+// run executes the `start deployment-manager-server` command.
+func (c *DeploymentManagerServerCommand) run(cmd *cobra.Command, argv []string) error {
+	// Get the context:
+	ctx := cmd.Context()
+
+	// Get the dependencies from the context:
+	logger := internal.LoggerFromContext(ctx)
+
+	// Get the flags:
+	flags := cmd.Flags()
+
+	// Get the cloud identifier:
+	cloudID, err := flags.GetString(cloudIDFlagName)
+	if err != nil {
+		logger.Error(
+			"Failed to get cloud identifier flag",
+			"flag", cloudIDFlagName,
+			"error", err.Error(),
+		)
+		return exit.Error(1)
+	}
+	if cloudID == "" {
+		logger.Error(
+			"Cloud identifier is empty",
+			"flag", cloudIDFlagName,
+		)
+		return exit.Error(1)
+	}
+	logger.Info(
+		"Cloud identifier",
+		"value", cloudID,
+	)
+
+	// Get the backend details:
+	backendTypeText, err := flags.GetString(backendTypeFlagName)
+	if err != nil {
+		logger.Error(
+			"Failed to get backend type flag",
+			"flag", backendTypeFlagName,
+			"error", err.Error(),
+		)
+		return exit.Error(1)
+	}
+	backendType := service.DeploymentManagerBackendType(backendTypeText)
+	switch backendType {
+	case service.DeploymentManagerBackendTypeGlobalHub:
+	case service.DeploymentManagerBackendTypeRegularHub:
+	default:
+		logger.Error(
+			"Unknown backend type",
+			slog.String("type", backendTypeText),
+		)
+		return exit.Error(1)
+	}
+	backendURL, err := flags.GetString(backendURLFlagName)
+	if err != nil {
+		logger.Error(
+			"Failed to get backend URL flag",
+			"flag", backendURLFlagName,
+			"error", err.Error(),
+		)
+		return exit.Error(1)
+	}
+	if backendURL == "" {
+		logger.Error(
+			"Backend URL is empty",
+			"flag", backendURLFlagName,
+		)
+		return exit.Error(1)
+	}
+	backendToken, err := flags.GetString(backendTokenFlagName)
+	if err != nil {
+		logger.Error(
+			"Failed to get backend token flag",
+			"flag", backendTokenFlagName,
+			"error", err.Error(),
+		)
+		return exit.Error(1)
+	}
+	if backendToken == "" {
+		logger.Error(
+			"Backend token is empty",
+			"flag", backendTokenFlagName,
+		)
+		return exit.Error(1)
+	}
+	extensions, err := flags.GetStringArray(extensionsFlagName)
+	if err != nil {
+		logger.Error(
+			"Failed to extension flag",
+			"flag", extensionsFlagName,
+			"error", err.Error(),
+		)
+		return exit.Error(1)
+	}
+	logger.Info(
+		"Backend details",
+		slog.String("type", string(backendType)),
+		slog.String("url", backendURL),
+		slog.String("!token", backendToken),
+		slog.Any("extensions", extensions),
+	)
+
+	// Create the logging wrapper:
+	loggingWrapper, err := logging.NewTransportWrapper().
+		SetLogger(logger).
+		SetFlags(flags).
+		Build()
+	if err != nil {
+		logger.Error(
+			"Failed to create transport wrapper",
+			"error", err.Error(),
+		)
+		return exit.Error(1)
+	}
+
+	// Create the authentication and authorization wrappers:
+	authenticationWrapper, err := authentication.NewHandlerWrapper().
+		SetLogger(logger).
+		SetFlags(flags).
+		Build()
+	if err != nil {
+		logger.Error(
+			"Failed to create authentication wrapper",
+			slog.String("error", err.Error()),
+		)
+		return exit.Error(1)
+	}
+	authorizationWrapper, err := authorization.NewHandlerWrapper().
+		SetLogger(logger).
+		SetFlags(flags).
+		Build()
+	if err != nil {
+		logger.Error(
+			"Failed to create authorization wrapper",
+			slog.String("error", err.Error()),
+		)
+		return exit.Error(1)
+	}
+
+	// Create the router:
+	router := mux.NewRouter()
+	router.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		service.SendError(w, http.StatusNotFound, "Not found")
+	})
+	router.MethodNotAllowedHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		service.SendError(w, http.StatusMethodNotAllowed, "Method not allowed")
+	})
+	router.Use(authenticationWrapper, authorizationWrapper)
+
+	// Create the handler:
+	handler, err := service.NewDeploymentManagerHandler().
+		SetLogger(logger).
+		SetLoggingWrapper(loggingWrapper).
+		SetCloudID(cloudID).
+		SetExtensions(extensions...).
+		SetBackendType(backendType).
+		SetBackendURL(backendURL).
+		SetBackendToken(backendToken).
+		SetEnableHack(true).
+		Build()
+	if err != nil {
+		logger.Error(
+			"Failed to create handler",
+			"error", err,
+		)
+		return exit.Error(1)
+	}
+
+	// Create the routes:
+	adapter, err := service.NewAdapter().
+		SetLogger(logger).
+		SetPathVariables("deploymentManagerID").
+		SetHandler(handler).
+		Build()
+	if err != nil {
+		logger.Error(
+			"Failed to create adapter",
+			"error", err,
+		)
+		return exit.Error(1)
+	}
+	router.Handle(
+		"/o2ims-infrastructureInventory/{version}/deploymentManagers",
+		adapter,
+	).Methods(http.MethodGet)
+	router.Handle(
+		"/o2ims-infrastructureInventory/{version}/deploymentManagers/{deploymentManagerID}",
+		adapter,
+	).Methods(http.MethodGet)
+
+	// Start the API server:
+	apiListener, err := network.NewListener().
+		SetLogger(logger).
+		SetFlags(flags, network.APIListener).
+		Build()
+	if err != nil {
+		logger.Error(
+			"Failed to to create API listener",
+			slog.String("error", err.Error()),
+		)
+		return exit.Error(1)
+	}
+	logger.Info(
+		"API listening",
+		slog.String("address", apiListener.Addr().String()),
+	)
+	apiServer := http.Server{
+		Addr:    apiListener.Addr().String(),
+		Handler: router,
+	}
+	err = apiServer.Serve(apiListener)
+	if err != nil {
+		logger.Error(
+			"API server finished with error",
+			slog.String("error", err.Error()),
+		)
+		return exit.Error(1)
+	}
+
+	return nil
+}
+
+// Names of command line flags:
+const (
+	backendTypeFlagName  = "backend-type"
+	backendTokenFlagName = "backend-token"
+	backendURLFlagName   = "backend-url"
+)
---
+++ internal/service/alert_subscription_handler.go
@@ -0,0 +1,266 @@
+/*
+Copyright 2023 Red Hat Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is
+distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the License for the specific language governing permissions and limitations under the
+License.
+*/
+
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	neturl "net/url"
+	"slices"
+	"sync"
+
+	jsoniter "github.com/json-iterator/go"
+
+	"github.com/openshift-kni/oran-o2ims/internal/data"
+	"github.com/openshift-kni/oran-o2ims/internal/jq"
+	"github.com/openshift-kni/oran-o2ims/internal/k8s"
+	"github.com/openshift-kni/oran-o2ims/internal/search"
+)
+
+// DeploymentManagerHandlerBuilder contains the data and logic needed to create a new deployment
+// manager collection handler. Don't create instances of this type directly, use the
+// NewDeploymentManagerHandler function instead.
+type AlertSubscriptionHandlerBuilder struct {
+	logger         *slog.Logger
+	loggingWrapper func(http.RoundTripper) http.RoundTripper
+	cloudID        string
+	extensions     []string
+}
+
+// AlertSubscriptionCollectionHander knows how to respond to requests to list deployment managers.
+// Don't create instances of this type directly, use the NewAlertSubscriptionHandler function
+// instead.
+type AlertSubscriptionHandler struct {
+	logger                   *slog.Logger
+	loggingWrapper           func(http.RoundTripper) http.RoundTripper
+	cloudID                  string
+	extensions               []string
+	jsonAPI                  jsoniter.API
+	selectorEvaluator        *search.SelectorEvaluator
+	jqTool                   *jq.Tool
+	subscritionMapMemoryLock *sync.Mutex
+	subscriptionMap          map[string]data.Object
+}
+
+// NewAlertSubscriptionHandler creates a builder that can then be used to configure and create a
+// handler for the collection of deployment managers.
+func NewAlertSubscriptionHandler() *AlertSubscriptionHandlerBuilder {
+	return &AlertSubscriptionHandlerBuilder{}
+}
+
+// SetLogger sets the logger that the handler will use to write to the log. This is mandatory.
+func (b *AlertSubscriptionHandlerBuilder) SetLogger(
+	value *slog.Logger) *AlertSubscriptionHandlerBuilder {
+	b.logger = value
+	return b
+}
+
+// SetLoggingWrapper sets the wrapper that will be used to configure logging for the HTTP clients
+// used to connect to other servers, including the backend server. This is optional.
+func (b *AlertSubscriptionHandlerBuilder) SetLoggingWrapper(
+	value func(http.RoundTripper) http.RoundTripper) *AlertSubscriptionHandlerBuilder {
+	b.loggingWrapper = value
+	return b
+}
+
+// SetCloudID sets the identifier of the O-Cloud of this handler. This is mandatory.
+func (b *AlertSubscriptionHandlerBuilder) SetCloudID(
+	value string) *AlertSubscriptionHandlerBuilder {
+	b.cloudID = value
+	return b
+}
+
+// SetExtensions sets the fields that will be added to the extensions.
+func (b *AlertSubscriptionHandlerBuilder) SetExtensions(
+	values ...string) *AlertSubscriptionHandlerBuilder {
+	b.extensions = values
+	return b
+}
+
+// Build uses the data stored in the builder to create and configure a new handler.
+func (b *AlertSubscriptionHandlerBuilder) Build() (
+	result *AlertSubscriptionHandler, err error) {
+	// Check parameters:
+	if b.logger == nil {
+		err = errors.New("logger is mandatory")
+		return
+	}
+	if b.cloudID == "" {
+		err = errors.New("cloud identifier is mandatory")
+		return
+	}
+
+	// Prepare the JSON iterator API:
+	jsonConfig := jsoniter.Config{
+		IndentionStep: 2,
+	}
+	jsonAPI := jsonConfig.Froze()
+
+	// Create the filter expression evaluator:
+	pathEvaluator, err := search.NewPathEvaluator().
+		SetLogger(b.logger).
+		Build()
+	if err != nil {
+		return
+	}
+	selectorEvaluator, err := search.NewSelectorEvaluator().
+		SetLogger(b.logger).
+		SetPathEvaluator(pathEvaluator.Evaluate).
+		Build()
+	if err != nil {
+		return
+	}
+
+	// Create the jq tool:
+	jqTool, err := jq.NewTool().
+		SetLogger(b.logger).
+		Build()
+	if err != nil {
+		return
+	}
+
+	// Check that extensions are at least syntactically valid:
+	for _, extension := range b.extensions {
+		_, err = jqTool.Compile(extension)
+		if err != nil {
+			return
+		}
+	}
+
+	// Create and populate the object:
+	result = &AlertSubscriptionHandler{
+		logger:                   b.logger,
+		loggingWrapper:           b.loggingWrapper,
+		cloudID:                  b.cloudID,
+		extensions:               slices.Clone(b.extensions),
+		selectorEvaluator:        selectorEvaluator,
+		jsonAPI:                  jsonAPI,
+		jqTool:                   jqTool,
+		subscritionMapMemoryLock: &sync.Mutex{},
+		subscriptionMap:          map[string]data.Object{},
+	}
+
+	return
+}
+
+// List is the implementation of the collection handler interface.
+func (h *AlertSubscriptionHandler) List(ctx context.Context,
+	request *ListRequest) (response *ListResponse, err error) {
+	// Create the stream that will fetch the items:
+	var items data.Stream
+
+	// Transform the items into what we need:
+	items = data.Map(items, h.mapItem)
+
+	// Select only the items that satisfy the filter:
+	if request.Selector != nil {
+		items = data.Select(
+			items,
+			func(ctx context.Context, item data.Object) (result bool, err error) {
+				result, err = h.selectorEvaluator.Evaluate(ctx, request.Selector, item)
+				return
+			},
+		)
+	}
+
+	// Return the result:
+	response = &ListResponse{
+		Items: items,
+	}
+	return
+}
+
+// Get is the implementation of the object handler interface.
+func (h *AlertSubscriptionHandler) Get(ctx context.Context,
+	request *GetRequest) (response *GetResponse, err error) {
+
+
+    //to move to fetch function
+	// Fetch the item:
+	h.subscritionMapMemoryLock.Lock()
+	defer h.subscritionMapMemoryLock.Unlock()
+	item, ok := h.subscriptionMap[request.Variables[0]]
+	if not ok {
+		err = ErrNotFound
+		// use go defer
+		return
+	}
+
+	// Transform the object into what we need:
+	item, err = h.mapItem(ctx, item)
+	if err != nil {
+		return
+	}
+
+	// Return the result:
+	response = &GetResponse{
+		Object: item,
+	}
+
+	return
+}
+
+
+func (h *AlertSubscriptionHandler) fetchItem(ctx context.Context,
+	id string) (result data.Object, err error) {
+	// Currently the ACM global hub API that we use doesn't have a specific endpoint for
+	// retrieving a specific object, instead of that we need to fetch a list filtering with a
+	// label selector.
+	query := neturl.Values{}
+	query.Set("labelSelector", fmt.Sprintf("clusterID=%s", id))
+	query.Set("limit", "1")
+	response, err := h.doGet(ctx, "/global-hub-api/v1/managedclusters", query)
+	if err != nil {
+		return
+	}
+	stream, err := k8s.NewStream().
+		SetLogger(h.logger).
+		SetReader(response.Body).
+		Build()
+	if err != nil {
+		return
+	}
+	items, err := data.Collect(ctx, stream)
+	if err != nil {
+		return
+	}
+	if len(items) == 0 {
+		err = ErrNotFound
+		return
+	}
+	result = items[0]
+	return
+}
+
+func (h *AlertSubscriptionHandler) fetchItems(
+	ctx context.Context) (result data.Stream, err error) {
+	if err != nil {
+		return
+	}
+	result, err = k8s.NewStream().
+		SetLogger(h.logger).
+		SetReader(response.Body).
+		Build()
+	return
+}
+
+func (h *AlertSubscriptionHandler) mapItem(ctx context.Context,
+	input data.Object) (output data.Object, err error) {
+
+	return input, nil
+}
