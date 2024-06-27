f554ccee9bfcf024bf64d47e2edefaee13e504da
Jennifer Chen <jenchen@redhat.com>
2024-06-27 14:47:13 -0400

    correct default configmap name for alarm

--- internal/service/constants.go
+++ internal/service/constants.go
@@ -3,7 +3,7 @@ package service
 const (
 	//default namespace should be changed to official namespace when it is available
 	DefaultNamespace                   = "orantest"
-	DefaultAlarmConfigmapName          = "oran-alarms-sub"
+	DefaultAlarmConfigmapName          = "oran-o2ims-alarm-subscriptions"
 	DefaultInfraInventoryConfigmapName = "oran-infra-inventory-sub"
 	FieldOwner                         = "oran-o2ims"
 )
